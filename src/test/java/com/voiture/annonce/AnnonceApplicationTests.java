054cd81a33c4dab3bf4d59d7c5020e48b8f10fa4
Laura Ophélie <lauraophelie1@gmail.com>
2024-01-06 10:13:39 +0100

    first commit

---
+++ src/test/java/com/voiture/annonce/AnnonceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voiture.annonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnonceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
